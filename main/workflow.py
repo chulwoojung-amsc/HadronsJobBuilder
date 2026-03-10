262e5775ea715492d0eecdbc826d1046cdf1a287
Chulwoo Jung <chulwoo@bnl.gov>
2026-03-10 16:50:30 -0400

    Adjusting for quadro

--- main/workflow.py
+++ main/workflow.py
@@ -8,13 +8,26 @@ local_llm = ChatOpenAI(
     temperature=0
 )
 
+quadro_llm = ChatOpenAI(
+    model="gpt-oss-120b",
+    openai_api_key='ollama',
+    openai_api_base="http://localhost:8000/v1",
+    temperature=0
+)
+
+quadro_llm2 = ChatOpenAI(
+    model="gpt-oss-120b",
+    base_url="https://api.i2-core.american-science-cloud.org/",
+    temperature=0
+)
+
 amsc_llm_0t = ChatOpenAI(
     model="gpt-oss-120b",
     base_url="https://api.i2-core.american-science-cloud.org/",
     temperature=0
 )
 
-llm = amsc_llm_0t
+llm = quadro_llm
 
 query = input("Describe the observables you wish to compute: ")
 print(query)
